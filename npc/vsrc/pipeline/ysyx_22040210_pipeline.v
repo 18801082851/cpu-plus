c58234c9711f0904516174e3dd90e8ae3d0ee3b1
18801082851 <135930445+18801082851@users.noreply.github.com>
2023-06-08 11:27:21 +0800

    Update ysyx_22040210_pipeline.v

--- npc/vsrc/pipeline/ysyx_22040210_pipeline.v
+++ npc/vsrc/pipeline/ysyx_22040210_pipeline.v
@@ -1,11 +1,3 @@
-/*
- * @Author: WenJiaBao-2022E8020282071
- * @Date: 2022-09-27 00:11:49
- * @LastEditTime: 2022-11-30 15:18:10
- * @Description: 
- * 
- * Copyright (c) 2022 by WenJiaBao wenjiabao0919@163.com, All Rights Reserved. 
- */
 `include "../npc/vsrc/include/ysyx_22040210_define.v"
 
  module ysyx_22040210_pipeline(
