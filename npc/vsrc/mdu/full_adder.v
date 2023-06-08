781faa0953514a873d0d2e976f09767a13639123
18801082851 <135930445+18801082851@users.noreply.github.com>
2023-06-08 11:28:28 +0800

    Update full_adder.v

--- npc/vsrc/mdu/full_adder.v
+++ npc/vsrc/mdu/full_adder.v
@@ -1,12 +1,3 @@
-/*
- * @Author: WenJiaBao-2022E8020282071
- * @Date: 2022-11-01 19:48:55
- * @LastEditTime: 2022-11-06 20:49:04
- * @Description: 
- * 
- * Copyright (c) 2022 by WenJiaBao wenjiabao0919@163.com, All Rights Reserved. 
- */
-
 module full_adder(
     input a,
     input b,
