4b400bcb2a65e9e5eee364a8553e215ea4b3589f
18801082851 <135930445+18801082851@users.noreply.github.com>
2023-06-08 11:26:38 +0800

    Update ysyx_22040210_id.v

--- npc/vsrc/pipeline/ysyx_22040210_id.v
+++ npc/vsrc/pipeline/ysyx_22040210_id.v
@@ -1,11 +1,3 @@
-/*
- * @Author: WenJiaBao-2022E8020282071
- * @Date: 2022-09-26 11:09:44
- * @LastEditTime: 2022-10-29 22:23:35
- * @Description: 
- * 
- * Copyright (c) 2022 by WenJiaBao wenjiabao0919@163.com, All Rights Reserved. 
- */
 `include "../npc/vsrc/include/ysyx_22040210_define.v"
  module ysyx_22040210_id (
 
